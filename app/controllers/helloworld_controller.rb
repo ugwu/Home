160fc79f8bbf913256575f3d39e67a69db7d51ad
Patrick <patrick.ugwu@mit-can.com>
2010-11-30 12:59:17 +0000

    Initial commit

---
+++ app/controllers/helloworld_controller.rb
@@ -0,0 +1,19 @@
+class HelloworldController < ApplicationController
+  
+  def index
+    @user = "Patrick is the best"
+    
+    @grades = { "Patrick" => " 100 ",
+               "Jim" =>  " 120 ",
+               "Billy" => " 58 "
+              }
+    
+    # render :text => "Patrick il é maestro del mondo"
+  end
+
+  def gen
+    @name = "Maestro"
+    
+  end
+  
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Home::Application.config.secret_token = 'a1e058b644f445e1546fc6db16bb5277e21b740e511d714c5c88682690c906fa7d1b333a9bd4854d924442a08454e2c88eca8e26dfcff60ccccefb34581bc360'
