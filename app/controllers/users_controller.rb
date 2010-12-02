04d0861786d2122a65aaadbe4ea2283d0139fa83
Patrick <patrick.ugwu@mit-can.com>
2010-12-02 16:27:57 +0000

    This is a working branch

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,18 +2,20 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.xml
   def index
-    @users = User.all
+    @users = User.find(:all)
 
     respond_to do |format|
-      format.html # index.html.erb
+      format.html # {index.html.erb}
       format.xml  { render :xml => @users }
-    end
+      format.json { render :json => @users }
+    end    
   end
 
   # GET /users/1
   # GET /users/1.xml
   def show
     @user = User.find(params[:id])
+  
 
     respond_to do |format|
       format.html # show.html.erb
